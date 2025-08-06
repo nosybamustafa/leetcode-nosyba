fde857a9edbb6289faa47d3224256c99693bb4d7
Nosyba Mustafa <152062932+nosybamustafa@users.noreply.github.com>
2025-08-06 15:21:32 +0300

    Time: 2 ms (68.74%) | Memory: 23.5 MB (31.66%) - LeetSync

---
+++ 155-min-stack/min-stack.cpp
@@ -0,0 +1,35 @@
+#include <stack>    
+using namespace std;
+
+class MinStack {
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        dataStack.push(val);
+        if (minStack.empty() || val <= minStack.top()) {
+            minStack.push(val);
+        }
+    }
+    
+    void pop() {
+        if (dataStack.top() == minStack.top()) {
+            minStack.pop();
+        }
+        dataStack.pop();
+    }
+    
+    int top() {
+        return dataStack.top();
+    }
+    
+    int getMin() {
+        return minStack.top();
+    }
+
+private:
+    stack<int> dataStack;  
+    stack<int> minStack;    
+};
